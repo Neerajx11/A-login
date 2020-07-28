e63b2669ee5c26ace6ae60f44d256a9f2d4255f5
Neeeraj <mauryaneeraj11@gmail.com>
2020-07-28 19:34:01 +0530

    added signUp page & UI fixes 28/07/2020

---
+++ SignUp/signUp.js
@@ -0,0 +1,15 @@
+$('.shift-signUp').on('click', function () {
+    $(this).toggleClass('fa-eye-slash');
+
+    let signUppass = document.getElementById('sp_pass');
+    (signUppass.type == 'password') ? (signUppass.setAttribute("type", "text")) : (signUppass.setAttribute("type", "password"));
+});
+
+// Toggle year and branch while register
+$('#ifStudent').hide();
+$('.op-show').change(function () {
+    ($(this).prop("checked")) ? $('#ifStudent').slideDown('fast') : '';
+});
+$('.op-hide').change(function () {
+    ($(this).prop("checked")) ? $('#ifStudent').slideUp('fast') : '';
+});
