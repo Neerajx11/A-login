8c4f0a8c3d5c962e3218e6a7366b818c1fd366fe
Neeraj Maurya <mauryaneeraj11@gmail.com>
2022-12-04 18:31:33 +0530

    format

--- SignUp/signUp.js
+++ SignUp/signUp.js
@@ -1,15 +1,17 @@
-$('.shift-signUp').on('click', function () {
-    $(this).toggleClass('fa-eye-slash');
+$(".shift-signUp").on("click", function () {
+  $(this).toggleClass("fa-eye-slash");
 
-    let signUppass = document.getElementById('sp_pass');
-    (signUppass.type == 'password') ? (signUppass.setAttribute("type", "text")) : (signUppass.setAttribute("type", "password"));
+  let signUppass = document.getElementById("sp_pass");
+  signUppass.type == "password"
+    ? signUppass.setAttribute("type", "text")
+    : signUppass.setAttribute("type", "password");
 });
 
 // Toggle year and branch while register
-$('#ifStudent').hide();
-$('.op-show').change(function () {
-    ($(this).prop("checked")) ? $('#ifStudent').slideDown('fast') : '';
+$("#ifStudent").hide();
+$(".op-show").change(function () {
+  $(this).prop("checked") ? $("#ifStudent").slideDown("fast") : "";
 });
-$('.op-hide').change(function () {
-    ($(this).prop("checked")) ? $('#ifStudent').slideUp('fast') : '';
+$(".op-hide").change(function () {
+  $(this).prop("checked") ? $("#ifStudent").slideUp("fast") : "";
 });
